b7eec67d973e367f0e8f3b78656a5318c5dde302
labocho <labocho@penguinlab.jp>
2022-09-27 18:18:36 +0900

    implement

--- lib/action_mailer_interceptor.rb
+++ lib/action_mailer_interceptor.rb
@@ -1,6 +1,46 @@
 require "action_mailer_interceptor/version"
+require "action_mailer"
+
+ActionMailer::Base.class_eval do
+  class << self
+    attr_accessor :interceptor
+  end
+end
 
 module ActionMailerInterceptor
   class Error < StandardError; end
-  # Your code goes here...
+
+  def deliver_now
+    intercept
+    super
+  end
+
+  def deliver_now!
+    intercept
+    super
+  end
+
+  private
+  def intercept
+    return unless interceptor
+
+    self.subject += original_receivers_description
+    self.to = interceptor
+    self.cc = nil
+    self.bcc = nil
+  end
+
+  def interceptor
+    ActionMailer::Base.interceptor
+  end
+
+  def original_receivers_description
+    receivers = ""
+    receivers << "To: #{to.join(", ")}"
+    receivers << "; Cc: #{cc.join(", ")}" if cc
+    receivers << "; Bcc: #{bcc.join(", ")}" if bcc
+    " [Originally sent #{receivers}]"
+  end
 end
+
+ActionMailer::MessageDelivery.prepend(ActionMailerInterceptor)
--- spec/action_mailer_interceptor_spec.rb
+++ spec/action_mailer_interceptor_spec.rb
@@ -1,9 +1,61 @@
 RSpec.describe ActionMailerInterceptor do
+  before do
+    ActionMailer::Base.delivery_method = :test
+  end
+
+  let(:mailer) {
+    Class.new(ActionMailer::Base) do
+      default from: "from@example.com"
+
+      def test(subject: "Test", to: "to@example.com")
+        mail(
+          to: to,
+          subject: subject,
+          body: "Body",
+          cc: "cc@example.com",
+          bcc: "bcc@example.com",
+        )
+      end
+    end
+  }
+
   it "has a version number" do
     expect(ActionMailerInterceptor::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  context "ActionMailer::Base.interceptor is nil" do
+    before do
+      expect(ActionMailer::Base.interceptor).to be_nil
+    end
+
+    it "delivers original recipient" do
+      mailer.test.deliver_now
+      expect(mailer.deliveries.last.to).to eq ["to@example.com"]
+      expect(mailer.deliveries.last.cc).to eq ["cc@example.com"]
+      expect(mailer.deliveries.last.bcc).to eq ["bcc@example.com"]
+    end
+
+    it "delivers with original subject" do
+      mailer.test.deliver_now
+      expect(mailer.deliveries.last.subject).to eq "Test"
+    end
+  end
+
+  context "ActionMailer::Base.interceptor is not nil" do
+    before do
+      ActionMailer::Base.interceptor = "interceptor@example.com"
+    end
+
+    it "delivers only to interceptor" do
+      mailer.test.deliver_now
+      expect(mailer.deliveries.last.to).to eq ["interceptor@example.com"]
+      expect(mailer.deliveries.last.cc).to eq nil
+      expect(mailer.deliveries.last.bcc).to eq nil
+    end
+
+    it "delivers with subject includes original recipient information" do
+      mailer.test.deliver_now
+      expect(mailer.deliveries.last.subject).to eq "Test [Originally sent To: to@example.com; Cc: cc@example.com; Bcc: bcc@example.com]"
+    end
   end
 end
